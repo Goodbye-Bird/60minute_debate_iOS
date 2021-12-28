6a1c7d06ce8a636b816117e2653e8a80c041f36a
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-12-28 17:43:05 +0900

    .
    .

--- 60_debate_iOS/60_debate_iOS/Source/UI/Login/LoginViewController.swift
+++ 60_debate_iOS/60_debate_iOS/Source/UI/Login/LoginViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var moveToSignUpView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func moveToSignUpView(_ sender: UIButton) {
+    }
+    
 }
--- 60_debate_iOS/60_debate_iOS/Source/UI/Signin/SigninViewController.swift
+++ 60_debate_iOS/60_debate_iOS/Source/UI/Signin/SigninViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class SigninViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = .systemBackground
         // Do any additional setup after loading the view.
     }
 
