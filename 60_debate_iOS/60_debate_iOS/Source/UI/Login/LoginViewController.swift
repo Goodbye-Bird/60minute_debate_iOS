3ea42099e978516ea870a020383e264d2d91a999
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-12-28 11:26:37 +0900

    Start:: 진짜 시작
    .

---
+++ 60_debate_iOS/60_debate_iOS/Source/UI/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  60_debate_iOS
+//
+//  Created by 김상은 on 2021/12/28.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
