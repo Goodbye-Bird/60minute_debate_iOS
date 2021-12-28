9488df270f84fb7ef1a0e48c73bf6172d822228e
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-12-28 11:41:13 +0900

    .
    .

---
+++ 60_debate_iOS/60_debate_iOS/Source/UI/Main/ChatView/ChatViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ChatViewController.swift
+//  60_debate_iOS
+//
+//  Created by 김상은 on 2021/12/28.
+//
+
+import Foundation
---
+++ 60_debate_iOS/60_debate_iOS/Source/UI/Main/ListView/ListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewController.swift
+//  60_debate_iOS
+//
+//  Created by 김상은 on 2021/12/28.
+//
+
+import Foundation
