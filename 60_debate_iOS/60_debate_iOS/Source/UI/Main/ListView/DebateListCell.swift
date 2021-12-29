c1aa99683929809eab5ac2e2fb5c30932947fa2d
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-12-29 10:46:31 +0900

    [REFACTOR] resolve conflict

---
+++ 60_debate_iOS/60_debate_iOS/Source/UI/Main/ListView/DebateListCell.swift
@@ -0,0 +1,23 @@
+//
+//  DebateListCell.swift
+//  60_debate_iOS
+//
+//  Created by 손민 on 2021/12/29.
+//
+
+import UIKit
+
+class DebateListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
