2a10547a811f63a3902aa9a3719997ddd19ba6a8
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-12-29 18:04:35 +0900

    .
    .

--- 60_debate_iOS/60_debate_iOS/Source/Network/LoginAPI.swift
+++ 60_debate_iOS/60_debate_iOS/Source/Network/LoginAPI.swift
@@ -14,6 +14,12 @@ extension LoginViewController {
     func LoginNetworking() {
         let completion: ((Login?) -> Void) = { data in
             debugPrint("😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀")
+            guard let status = data?.status else { return }
+            if status == 200 {
+                let storyboard = UIStoryboard(name: "List", bundle: nil)
+                guard let VC = storyboard.instantiateViewController(withIdentifier: "List") as? ListViewController else { return }
+                UIApplication.shared.keyWindow?.replaceRootViewController(VC, animated: true, completion: nil)
+            }
         }
         let parameters = ["email": emailTextField.text!, "password": passwordTextField.text!]
         APImanager.doRequest("\(Constants.SERVER_IP)/auth/login", method: .post, parameters: parameters, completion: completion)
